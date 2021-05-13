824b53e1fd1ed08594fdec79b3a9416851096a38
PRPA1984 <m.p.a@live.com.ar>
2021-05-13 12:47:23 -0300

    Mejorando

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,29 +3,21 @@ class UsersController < ApplicationController
     before_action :validateCurrentUserNotBlank, only: [:logout, :current]
 
     def login
-        begin
-            username = user_params[:username]
-            password = user_params[:password]
-            user = User.find_by(username: username, password: password)
-            if user.present?
-                token = user.generateToken
-                user.save!
-                return render(json: {"token": token}, status: 200)
-            else
-                return render(json: formatError("User not found"), status: 400)
-            end
-        rescue => exception
-            return render(json: formatError(current_user.errors.full_messages.join(", ")), status:400)
+        username = user_params[:username]
+        password = user_params[:password]
+        user = User.find_by(username: username, password: password)
+        if user.present?
+            token = user.generateToken
+            user.save
+            return render(json: {"token": token}, status: 200)
+        else
+            return render(json: formatError("User not found"), status: 400)
         end
     end
 
     def logout
-        begin
-            current_user.update!(token: nil)
-            return render(status:200)
-        rescue => exception
-            return render(json: formatError(current_user.errors.full_messages.join(", ")), status:400)
-        end
+        current_user.update!(token: nil)
+        return render(status:200)
     end
 
     def create
